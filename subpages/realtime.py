21d00b7b158110107c6d2dfaceb36f499faaf6c4
DaoChaShao <27882272@qq.com>
2025-09-27 15:11:26 +0800

    feat: add realtime.py with initial metadata and description

---
+++ subpages/realtime.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3.12
+# -*- Coding: UTF-8 -*-
+# @Time     :   2025/9/27 15:03
+# @Author   :   Shawn
+# @Version  :   Version 0.1.0
+# @File     :   realtime.py
+# @Desc     :   
+
+
