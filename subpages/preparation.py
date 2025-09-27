11cd7c584a5bb6c9d66d3fe593891fa4c922440b
DaoChaShao <27882272@qq.com>
2025-09-27 15:11:06 +0800

    feat: add preparation.py with initial metadata and description

---
+++ subpages/preparation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3.12
+# -*- Coding: UTF-8 -*-
+# @Time     :   2025/9/27 14:59
+# @Author   :   Shawn
+# @Version  :   Version 0.1.0
+# @File     :   preparation.py
+# @Desc     :   
+
+
