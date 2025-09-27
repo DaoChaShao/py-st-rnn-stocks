bc4cff93aa337a2eb2f66f5d3f782acea8dc81c6
DaoChaShao <27882272@qq.com>
2025-09-27 15:10:36 +0800

    feat: add config.py with initial metadata and description

---
+++ utlis/config.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3.12
+# -*- Coding: UTF-8 -*-
+# @Time     :   2025/9/27 14:58
+# @Author   :   Shawn
+# @Version  :   Version 0.1.0
+# @File     :   config.py
+# @Desc     :   
+
