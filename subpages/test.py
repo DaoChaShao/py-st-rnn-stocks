471e83f148b1dc997058fc0e48000f9ca537daa7
DaoChaShao <27882272@qq.com>
2025-09-28 18:14:17 +0800

    refactor: simplify session state management and clean up unused code in test.py

--- subpages/test.py
+++ subpages/test.py
@@ -11,7 +11,7 @@ from os import path
 from sklearn.metrics import mean_absolute_error, mean_squared_error, root_mean_squared_error, r2_score
 from streamlit import (empty, sidebar, subheader, session_state, multiselect, caption, slider,
                        button, spinner, rerun,
-                       columns, markdown, metric)
+                       columns, metric)
 from tensorflow.keras import models, metrics
 
 from utils.config import SAVE_MODEL_PATH
@@ -26,7 +26,7 @@ emptu_norm_chart: empty = empty()
 load_sessions: list[str] = ["loaded_model", "loadTimer"]
 for load_session in load_sessions:
     session_state.setdefault(load_session, None)
-norm_sessions: list[str] = ["norm", "y_norm"]
+norm_sessions: list[str] = ["norm"]
 for session in norm_sessions:
     session_state.setdefault(session, None)
 pro_sessions: list[str] = ["X_test", "y_test"]
@@ -52,15 +52,7 @@ with sidebar:
                     with spinner("Loading Trained Model...", show_time=True, width="stretch"):
                         with Timer("Load the trained model") as session_state["loadTimer"]:
                             session_state["loaded_model"] = models.load_model(SAVE_MODEL_PATH, compile=False)
-                            # session_state["loaded_model"].compile(
-                            #     optimizer="adam",
-                            #     loss="mse",
-                            #     metrics=[
-                            #         metrics.MeanAbsoluteError(name="mae"),
-                            #         metrics.MeanSquaredError(name="mse"),
-                            #         metrics.RootMeanSquaredError(name="rmse"),
-                            #     ]
-                            # )
+                            # session_state["loaded_model"] = session_state["model"]
                     rerun()
             else:
                 if session_state["y_pred"] is None:
@@ -88,14 +80,6 @@ with sidebar:
                         r2 = r2_score(session_state["y_test"], session_state["y_pred"])
                         metric("R2", f"{r2:.3f}", delta=None, delta_color="off")
 
-                    y_norm: list[str] = multiselect(
-                        "Select the feature to be trained",
-                        options=session_state["norm"].columns.tolist(),
-                        default=session_state["y_norm"], disabled=True,
-                        help="Select the feature to be trained for RNN model.",
-                    )
-                    caption(f"Note: the feature(s) is/are {", ".join(session_state["y_norm"])}")
-
                     start, end = slider(
                         "Select Index Range to View",
                         min_value=0,
@@ -105,8 +89,8 @@ with sidebar:
                     )
 
                     comparison: DataFrame = DataFrame({
-                        f"{', '.join(session_state["y_norm"])} Ture": session_state["y_test"].flatten(),
-                        f"{', '.join(session_state["y_norm"])} Pred": session_state["y_pred"].flatten(),
+                        "Ture": session_state["y_test"].flatten(),
+                        "Pred": session_state["y_pred"].flatten(),
                     }).iloc[start: end + 1]
                     empty_norm_title.markdown("#### The Test Chart")
                     emptu_norm_chart.line_chart(comparison, width="stretch")
